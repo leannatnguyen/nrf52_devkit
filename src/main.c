04ca1088641b350164bb0a73143898e1905ce10a
Leanna Nguyen <leanna.t.nguyen@gmail.com>
2025-09-09 18:38:11 -0700

    adding in message queue and adding sensor data to message queue

--- src/main.c
+++ src/main.c
@@ -9,11 +9,19 @@
 #define MY_THREAD_STACK_SIZE 1024
 #define MY_THREAD_PRIORITY 5
 
+struct sensor_message {
+        float temp;
+        float humidity;
+};
+
 LOG_MODULE_REGISTER(main);
+
 K_THREAD_STACK_DEFINE(my_sensor_thread, MY_THREAD_STACK_SIZE);
 // K_THREAD_STACK_DEFINE(my_uart_thread, MY_THREAD_STACK_SIZE);
 static struct k_thread my_sensor_thread_data;
 // static struct k_thread my_uart_thread_data;
+
+K_MSGQ_DEFINE(sensor_queue, sizeof(struct sensor_message), 5, 4);
 
 void my_sensor_thread_function(void *device_ptr, void *p2, void *p3) {
         // Cast device's pointer back to the correct type
@@ -25,23 +33,35 @@ void my_sensor_thread_function(void *device_ptr, void *p2, void *p3) {
         }
         LOG_INF("Sensor is ready.");
 
-        while (1) {
-                // struct sensor_value temp, humidity;
-                int ret = sensor_sample_fetch(sensor_device);
-                // int ret_humidity = sensor_sample_fetch(sensor_device);
+        struct sensor_message msg;
 
-                // if (ret_humidity < 0) {
-                //         LOG_ERR("Failed to fetch humidity.");
-                // } else {
-                //         LOG_INF("Humidty Fetched.");
-                // }
+        while (1) {
+                struct sensor_value temp, humidity;
+                int ret = sensor_sample_fetch(sensor_device);
+
                 if (ret < 0) {
                         LOG_ERR("Failed to fetch temperature and humidity.");
                 } else {
                         LOG_INF("Temperature and humidity Fetched.");
+
                 }
 
+                // Read humidity and temperature + add to message queue
+                int ret_humidity = sensor_channel_get(sensor_device, SENSOR_CHAN_HUMIDITY, &humidity);
+                int ret_temp= sensor_channel_get(sensor_device, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+                if (ret_humidity < 0) {
+                        LOG_ERR("Failed to get humidity. Error: %d", ret_humidity);
+                        break;
+                } else if(ret_temp < 0) {
+                        LOG_ERR("Failed to get temperature. Error: %d", ret_temp);
+                        break;
+                }
+                else {
+                        msg.temp = sensor_value_to_double(&temp);
+                        msg.humidity = sensor_value_to_double(&humidity);
 
+                        k_msgq_put(&sensor_queue, &msg, K_NO_WAIT);
+                }
 
                 k_sleep(K_MSEC(2000)); // sleep for 2 seconds
         }
@@ -65,20 +85,6 @@ int main(void)
                                 my_sensor_thread_function,(void *)sensor_device, NULL, 
                                 NULL,MY_THREAD_PRIORITY, 0, K_NO_WAIT);
 
-        // Read humidity and temperature
-        // int ret_humidity = sensor_channel_get(sensor_device, SENSOR_CHAN_HUMIDITY, &humidity);
-        // if (ret_humidity < 0) {
-        //         LOG_ERR("Failed to get humnidity. Error: %d", ret_humidity);
-        // } else {
-        //         LOG_INF("Humdity is %d.%06d%%", humidity.val1, humidity.val2);
-        // }
-
-        // int ret_temp= sensor_channel_get(sensor_device, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-        // if (ret_temp < 0) {
-        //         LOG_ERR("Failed to get temperature. Error: %d", ret_temp);
-        // } else {
-        //         LOG_INF("Temperature is %d.%06d°C", temp.val1, temp.val2);
-        // }
 
         while (1) 
         {
