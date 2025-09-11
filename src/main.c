09fa9359b65abae4327b9406eba0f5db7aafea63
Leanna Nguyen <leanna.t.nguyen@gmail.com>
2025-09-10 21:10:34 -0700

    added in logic to only read sensor if button enables logging

--- src/main.c
+++ src/main.c
@@ -33,126 +33,167 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static struct gpio_callback button_cb_data;
 
 void my_sensor_thread_function(void *device_ptr, void *p2, void *p3) {
-        // Cast device's pointer back to the correct type
-        const struct device *sensor_device = (const struct device *)device_ptr;
-        if (!device_is_ready(sensor_device)){
-                // handle error
-                LOG_ERR("Sensor is not ready");
-                return;
-        }
-        LOG_INF("Sensor is ready.");
+	// Cast device's pointer back to the correct type
+	const struct device *sensor_device = (const struct device *)device_ptr;
+	if (!device_is_ready(sensor_device)){
+		// handle error
+		LOG_ERR("Sensor is not ready");
+		return;
+	}
+	LOG_INF("Sensor is ready.");
 
-        struct sensor_message msg;
-        bool is_logging;
+	struct sensor_message msg;
+	bool is_logging;
 
-        while (1) {
-                k_mutex_lock(&logging_mutex, K_FOREVER);
-                is_logging = logging_enabled;
-                k_mutex_unlock(&logging_mutex);
-                struct sensor_value temp, humidity;
-                int ret = sensor_sample_fetch(sensor_device);
+	while (1) {
+		k_mutex_lock(&logging_mutex, K_FOREVER);
+		is_logging = logging_enabled;
+		k_mutex_unlock(&logging_mutex);
+		
+		if (is_logging) {
+			struct sensor_value temp, humidity;
+			int ret = sensor_sample_fetch(sensor_device);
 
-                if (ret < 0) {
-                        LOG_ERR("Failed to fetch temperature and humidity.");
-                } else {
-                        LOG_INF("Temperature and humidity Fetched.");
+			if (ret < 0) {
+				LOG_ERR("Failed to fetch temperature and humidity.");
+			} else {
+				LOG_INF("Temperature and humidity Fetched.");
 
-                }
+			}
 
-                // Read humidity and temperature + add to message queue
-                int ret_humidity = sensor_channel_get(sensor_device, SENSOR_CHAN_HUMIDITY, &humidity);
-                int ret_temp= sensor_channel_get(sensor_device, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-                if (ret_humidity < 0) {
-                        LOG_ERR("Failed to get humidity. Error: %d", ret_humidity);
-                        break;
-                } else if(ret_temp < 0) {
-                        LOG_ERR("Failed to get temperature. Error: %d", ret_temp);
-                        break;
-                }
-                else {
-                        msg.temp = sensor_value_to_double(&temp);
-                        msg.humidity = sensor_value_to_double(&humidity);
-                        // LOG_INF(......);
-                        k_msgq_put(&sensor_queue, &msg, K_NO_WAIT);
-                }
+			// Read humidity and temperature + add to message queue
+			int ret_humidity = sensor_channel_get(sensor_device, SENSOR_CHAN_HUMIDITY, &humidity);
+			int ret_temp= sensor_channel_get(sensor_device, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+			if (ret_humidity < 0) {
+				LOG_ERR("Failed to get humidity. Error: %d", ret_humidity);
+				break;
+			} else if(ret_temp < 0) {
+				LOG_ERR("Failed to get temperature. Error: %d", ret_temp);
+				break;
+			}
+			else {
+				msg.temp = sensor_value_to_double(&temp);
+				msg.humidity = sensor_value_to_double(&humidity);
+				// LOG_INF(......);
+				k_msgq_put(&sensor_queue, &msg, K_NO_WAIT);
+			}
+		}
 
-                k_sleep(K_MSEC(2000)); // sleep for 2 seconds
-        }
+		k_sleep(K_MSEC(2000)); // sleep for 2 seconds
+	}
 }
 
 void my_uart_thread_function(void *p1, void *p2, void *p3) {
-        struct sensor_message msg;
+	struct sensor_message msg;
 
-        while (1) {
-                int ret = k_msgq_get(&sensor_queue, &msg, K_FOREVER);
-                if (ret == 0) {
-                        LOG_INF("Temperature = %.2f C, Humidity = %.2f%%", (double)msg.temp, (double)msg.humidity);
-                }
-        }
+	while (1) {
+		int ret = k_msgq_get(&sensor_queue, &msg, K_FOREVER);
+		if (ret == 0) {
+			LOG_INF("Temperature = %.2f C, Humidity = %.2f%%", (double)msg.temp, (double)msg.humidity);
+		}
+	}
 }
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, gpio_port_pins_t pins)
 {
-        k_mutex_lock(&logging_mutex, K_FOREVER);
+	k_mutex_lock(&logging_mutex, K_FOREVER);
 
-        // toggle logging enabled since button was pressed
-        logging_enabled = !logging_enabled;
-        LOG_INF("Button pressed!");
-        if (logging_enabled) {
-                LOG_INF("Sensor logging enabled!");
-        } else {
-                LOG_INF("Sensor logging disabled!");
-        }
-        
-        k_mutex_unlock(&logging_mutex);
+	// toggle logging enabled since button was pressed
+	logging_enabled = !logging_enabled;
+	LOG_INF("Button pressed!");
+	if (logging_enabled) {
+		LOG_INF("Sensor logging enabled!");
+	} else {
+		LOG_INF("Sensor logging disabled!");
+	}
+	
+	k_mutex_unlock(&logging_mutex);
 }
 
 int main(void)
 {
-        LOG_INF("Starting up!");
-        
-        // Init mutex
-        k_mutex_init(&logging_mutex);
-        LOG_INF("Logging mutex initialized");
+	LOG_INF("Starting up!");
+	
+	// Init mutex
+	k_mutex_init(&logging_mutex);
+	LOG_INF("Logging mutex initialized");
 
-        // Get device
-        const struct device *sensor_device = DEVICE_DT_GET(MY_SENSOR_NODE);
+	// Get device
+	const struct device *sensor_device = DEVICE_DT_GET(MY_SENSOR_NODE);
 
-        // Check if button is ready
-        if (!gpio_is_ready_dt(&button)) {
-                LOG_ERR("Button is not ready.");
-                return 0;
-        }
-        LOG_INF("Button is ready.");
+	// Check if button is ready
+	if (!gpio_is_ready_dt(&button)) {
+		LOG_ERR("Button is not ready.");
+		return 0;
+	}
+	LOG_INF("Button is ready.");
 
-        // configure the button as an input
-        int ret_button = gpio_pin_configure_dt(&button, GPIO_INPUT);
-        ret_button = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-        if (ret_button != 0) {
-                LOG_ERR("Failed to configure button.");
-                return 0;
-        }
-        LOG_INF("Button configured as input.");
+	// configure the button as an input
+	int ret_button = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	ret_button = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret_button != 0) {
+		LOG_ERR("Failed to configure button.");
+		return 0;
+	}
+	LOG_INF("Button configured as input.");
 
-        // init callback
-        gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-        gpio_add_callback(button.port, &button_cb_data);
+	// init callback
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
 
-        // k_thread_create(&my_sensor_thread_data, my_sensor_thread, MY_THREAD_STACK_SIZE, 
-        //                         my_sensor_thread_function,(void *)sensor_device, NULL, 
-        //                         NULL,MY_THREAD_PRIORITY, 0, K_NO_WAIT);
+	k_thread_create(&my_sensor_thread_data, my_sensor_thread, MY_THREAD_STACK_SIZE, 
+						my_sensor_thread_function,(void *)sensor_device, NULL, 
+						NULL,MY_THREAD_PRIORITY, 0, K_NO_WAIT);
 
-        // k_thread_create(&my_uart_thread_data, my_uart_thread, MY_THREAD_STACK_SIZE, 
-        //                         my_uart_thread_function, NULL, NULL, 
-        //                         NULL, MY_THREAD_PRIORITY, 0, K_NO_WAIT);
+	k_thread_create(&my_uart_thread_data, my_uart_thread, MY_THREAD_STACK_SIZE, 
+                        my_uart_thread_function, NULL, NULL, 
+                        NULL, MY_THREAD_PRIORITY, 0, K_NO_WAIT);
 
-        while (1) 
-        {
-                k_sleep(K_MSEC(5000));
-                // LOG_INF("MAIN THREAD STILL RUNNING!\n");
-        }
-        
+	while (1) 
+	{
+		k_sleep(K_MSEC(5000));
+		// LOG_INF("MAIN THREAD STILL RUNNING!\n");
+	}
+	
 
-        return 0;
+	return 0;
 }
+
+/* Output Log 
+*** Booting nRF Connect SDK v2.7.0-5cb85570ca43 ***
+*** Using Zephyr OS v3.6.99-100befc70c74 ***
+[00:00:00.479,064] <inf> main: Starting up!
+[00:00:00.479,064] <inf> main: Logging mutex initialized
+[00:00:00.479,064] <inf> main: Button is ready.
+[00:00:00.479,095] <inf> main: Button configured as input.
+[00:00:00.479,125] <inf> main: Sensor is ready.
+[00:00:00.480,224] <inf> main: Temperature and humidity Fetched.
+[00:00:00.480,316] <inf> main: Temperature = 25.03 C, Humidity = 56.70%
+[00:00:02.481,445] <inf> main: Temperature and humidity Fetched.
+[00:00:02.481,536] <inf> main: Temperature = 25.06 C, Humidity = 57.01%
+[00:00:03.462,799] <inf> main: Button pressed!
+[00:00:03.462,799] <inf> main: Sensor logging disabled!
+[00:00:13.376,831] <inf> main: Button pressed!
+[00:00:13.376,861] <inf> main: Sensor logging enabled!
+[00:00:14.483,032] <inf> main: Temperature and humidity Fetched.
+[00:00:14.483,123] <inf> main: Temperature = 25.09 C, Humidity = 56.35%
+[00:00:16.484,252] <inf> main: Temperature and humidity Fetched.
+[00:00:16.484,344] <inf> main: Temperature = 25.07 C, Humidity = 56.25%
+[00:00:18.485,473] <inf> main: Temperature and humidity Fetched.
+[00:00:18.485,565] <inf> main: Temperature = 25.09 C, Humidity = 56.30%
+[00:00:20.486,694] <inf> main: Temperature and humidity Fetched.
+[00:00:20.486,785] <inf> main: Temperature = 25.09 C, Humidity = 56.12%
+[00:00:21.633,361] <inf> main: Button pressed!
+[00:00:21.633,392] <inf> main: Sensor logging disabled!
+[00:00:24.289,489] <inf> main: Button pressed!
+[00:00:24.289,489] <inf> main: Sensor logging enabled!
+[00:00:24.487,976] <inf> main: Temperature and humidity Fetched.
+[00:00:24.488,067] <inf> main: Temperature = 25.07 C, Humidity = 55.93%
+[00:00:26.489,196] <inf> main: Temperature and humidity Fetched.
+[00:00:26.489,288] <inf> main: Temperature = 25.06 C, Humidity = 55.90%
+[00:00:28.490,417] <inf> main: Temperature and humidity Fetched.
+[00:00:28.490,509] <inf> main: Temperature = 25.06 C, Humidity = 55.81%
+[00:00:30.451,995] <inf> main: Button pressed!
+[00:00:30.451,995] <inf> main: Sensor logging disabled!
+*/
 
